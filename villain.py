e510835a5a2c2a9cac4e254fda013af097bd4ec0
Will <willwtipton@gmail.com>
2019-06-20 20:36:03 -0700

    cache notes for efficiency

--- villain.py
+++ villain.py
@@ -4,15 +4,18 @@ class Villain:
   def __init__(self, name, data_dir):
     self.name = name
     self.path = os.path.join(data_dir, 'notes', name)
-
-  def GetNotes(self):
     try:
       with open(self.path) as f:
-        return f.read()
+        self.notes = f.read()
     except:
       print(self.name, " is new i guess!")
-      return ''
+      self.notes = ''
+
+  def GetNotes(self):
+    print (self.notes)
+    return self.notes
 
   def SaveNotes(self, notes):
+    self.notes = notes
     with open(self.path, 'w') as f:
-      f.write(notes)
+      f.write(self.notes)
